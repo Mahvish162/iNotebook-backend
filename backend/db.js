737b6a107ede3620fc349c69feab7ad883f4182a
Mahvish162 <mahvishfatima162@gmail.com>
2024-03-04 16:26:29 +0530

    vedio 45 completed

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,21 @@ const mongoose = require('mongoose');
 const mongoURI="mongodb://localhost:27017";
 
 async function connectToMongo() {
-    await mongoose.connect(mongoURI).then(()=> console.log("Connected to Mongo Successfully")).catch(err => console.log(err));
+    await mongoose.connect(mongoURI).then(()=> console.log("Connected to Mongo Successfully yep")).catch(err => console.log(err));
   }
-  
+
 module.exports=connectToMongo;
+
+/*
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+
+const connectToMongo = ()=>{
+    mongoose.connect(mongoURI, ()=>{
+        console.log("Connected to Mongo Successfully");
+    })
+}
+
+module.exports = connectToMongo;
+*/
--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,17 @@
-const connectToMongo=require('./db');
+const connectToMongo = require('./db');
+const express = require('express')
 
 connectToMongo();
-
-
-const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json())
+
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
