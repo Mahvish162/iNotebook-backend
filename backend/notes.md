5af47467ffe2e9eecb409c2b9631433fa3ce066e
Mahvish162 <mahvishfatima162@gmail.com>
2024-03-09 13:36:04 +0530

    vedio no. 49 completed

---
+++ backend/notes.md
@@ -0,0 +1,64 @@
+                  ##  MERN Stack 
+okay so lets get started for the new project named as inotebook :
+
+Basically we create this app just for the sake of learning and the vision or plan of this app's building is to create such an environment where users can be able to store and retrieve there data/notes, and these are stored in server on the cloud.
+
+here is the organized steps for creating this project :
+
+(1).  We create a Backend folder here in this particular folder, just for the sake of learning and we store our backend stuffs there.
+      use commands like :<br>
+         #npm init<br>
+         #npm i mongoose  
+         #npm i express. 
+
+# MONGOOSE: 
+ * Mongoose is described as “ elegant MongoDB object modeling for Node.js.” 
+   Mongoose is an ODM  (Object Data Modeling) library for MongoDB 
+ * MongoDB is a popular NoSQL database, and Mongoose acts as an abstraction layer over it.
+ * It provides a schema-based solution for interacting with MongoDB.
+ * Think of it as a bridge between your Node.js application and the MongoDB database.
+
+# EXPRESS :
+
+Express.js is the most popular web framework for Node.js. It is designed for building web applications and APIs and has been called the de facto standard server framework for Node.js.
+
+Building a backend from-scratch for an application in Node.js can be tedious and time consuming. From setting up ports to route handlers, writing all of the boilerplate code takes away from what really matters, which is writing the business logic for an application. By using web frameworks like Express.js, developers can save time and focus on other important tasks.
+
+(2) Creating Routes & Schema for Storing notes & users.
+
+(3) Storing Database using Mongoose model
+
+(4) Adding Data Validation Using express-validator
+
+(5) Creating Thunderclient collections to Manage Requests.
+
+(6) Understanding & implementing Password Hashing, Salt & Pepper, Usage of bcryptjs in Nodejs.
+
+## Hashing passwords using bcrypt in Nodejs
+
+* Simple plain passwords are not secured enough.
+* Therefore we use the hash generator method, which converts the simple plain paswords in hashed form.
+* And very helpful but not enough.
+As those hashed passwords are hacked by the hackers using rainbow table.
+* Therefore to overcome that, we use a salting method over the passwords, so that the random texts will be generated and then no hacking will be proceeded by there.
+
+
+## Token
+
+Types :
+(1) Session token
+(2) Json web token : Used in this course, described as the way to authenticate the particular user repeatedly.
+
+JWT serves as a very powerful fascinator of solid security & authentication between the server and the client.
+
+To leverage bcryptjs add npm package cld as jsonwebtoken and install it in ur npm package by following the command -->  "npm i jsonwebtoken".
+
+Parts of a token :
+
+(1) ALGORITHM & TOKEN TYPE<br>
+(2) Data<br>
+(3) Signature
+
+       
+ <b>website References</b>  : jsonwebtoken  /  jwt.io
+
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,49 +1,18 @@
-/*
-const express = require('express');
-const User = require('../models/User');
-const router = express.Router();
-
-const { body, validationResult } = require('express-validator'); 
-
-
-// Create a User using: POST "/api/auth/". Doesn't require Auth
-
-// adding validation layer in the inotebook
-router.post('/',[
-    body('name','enter a valid name').isLength({min:3}),
-    body('email','enter a valid email').isEmail(),
-    body('password','enter a valid password').isLength({min:5}),
-
-], (req, res)=>{ 
-    const result = validationResult(req);
-  if (!result.isEmpty()) {
-    return res.send(`Hello,${req.query.person}!`);
-   // return res.status(400).json({errors:errors.array()});
-}
-
-  res.send({ errors: result.array() });
-
-  User,create({
-    name:req.body.name,
-    password:req.body.password,
-    email:req.body.email,
-  }).then(user => res.json(user));
-    
-})
-
-module.exports = router
-
-*/
-
 
 //Those who are facing error after User.create , updated code that helped me resolve the error: 
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt=require('bcryptjs');   // bcrypt is used for storing the simple plain text password in hashed form in Nodejs
+var jwt = require('jsonwebtoken');
 
-// Create a user using POST "/api/auth", doesn't require authentication
-router.post('/', [
+const JWT_SECRET ='Mahvishisagoodg$irl';
+
+
+
+// Create a user using POST "/api/auth/createuser",No login required.
+router.post('/createuser', [
   body('name', 'Enter a valid name').isLength({ min: 3 }),
   body('email', "Enter a valid Email").isEmail(),
   body('password', 'Password must have a minimum of 5 characters').isLength({ min: 5 }),
@@ -53,24 +22,38 @@ router.post('/', [
     return res.status(400).json({ errors: errors.array() });
   }
 
+  const salt =await bcrypt.genSalt(10);  //Salting refers to adding random data to a hash function to obtain a unique output which refers to the hash. Even when the same input is used, it is possible to obtain different and unique hashes. 
+  const secPass=await bcrypt.hash(req.body.password,salt)  // visit jwt.io
   try {
     const user = await User.create({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password
+      password: secPass
     });
-    res.json(user);
+    const data={
+      user:{
+        id:user.id   // here we use id bcoz data of users have id. so we use any of then makes the same sense.
+      }
+    }
+    const authToken= jwt.sign(data,JWT_SECRET);      // uses the signature method of jwt.io and this returns a promise.
+   // console.log(jwtData)  // returns  a token in console.
+
+   // res.json(user);
+   
+   res.json('authToken :' + authToken)  
+   
+   // Now this command returns a token and by we can convert that token into the data as described in "data" object above. And by do so we can came to know about all such users those commit the modifications or tempering on the tokens third part i.e. SIGNATURE ,by having commands as jwt.verify(token, secretOrPublicKey, [options, callback]) from jsonwebtoken website & therefore can be in more safer side from any hackings. 
+ 
   } catch (error) {
     if (error.code === 11000) {
       // Duplicate key error
       return res.status(400).json({ error: 'Email already exists' });
     }
     console.error(error);
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ error: 'here i got Server error' });
   }
 });
 module.exports = router;
-
 
 
 
